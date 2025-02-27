5d0e17412f19bb5b716f4922b0fb4dd1f44b7ecc
Mayur <mayur72104@gmail.com>
2025-02-27 20:48:01 +0530

    firebase fixed

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -1,24 +1,21 @@
 // src/firebase.js
-import { initializeApp } from 'firebase/app';
-import { getAuth, GoogleAuthProvider } from 'firebase/auth';
+import { initializeApp } from "firebase/app";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAGivc2Sd2odtqiWSUO5zNjuc59Q-XoASQ",
-  authDomain: "temptourista.firebaseapp.com",
-  projectId: "temptourista",
-  storageBucket: "temptourista.firebasestorage.app",
-  messagingSenderId: "709061964602",
-  appId: "1:709061964602:web:3217a78f3ec5e6ac95d603"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const provider = new GoogleAuthProvider();
 provider.setCustomParameters({
-  prompt: 'select_account', // This will force the account selection prompt
+  prompt: "select_account", // This will force the account selection prompt
 });
 
-export { auth,provider };
-
-
-
+export { auth, provider };
